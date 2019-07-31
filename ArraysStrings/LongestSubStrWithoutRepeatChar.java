bba0ce308fc9c35e504f2ac38effe411f25588d4
Kartik Siddhabathula <ksiddhabathula@apple.com>
2019-07-31 08:43:51 -0700

    Program to find the length of the longest sub string without repeating characters

--- ArraysStrings/LongestSubStrWithoutRepeatChar.java
+++ ArraysStrings/LongestSubStrWithoutRepeatChar.java
@@ -1,15 +1,15 @@
 package programcreek.ArraysStrings;
 
-import java.util.HashSet;
-
 /**
  * 
  * @author kartik
  *	Program to find the length of the longest sub string without repeating characters
- */
+*/
+
+import java.util.HashSet;
 
 public class LongestSubStrWithoutRepeatChar {
-	
+
 	public static void main(String[] args) {
 		String str = "pwwke";
 		int res = lenSubStr(str);
@@ -17,28 +17,28 @@ public class LongestSubStrWithoutRepeatChar {
 	}
 
 	private static int lenSubStr(String str) {
-		if(str.length() <1) {
+		if (str.length() < 1) {
 			return 0;
 		}
-		int len=0,idx=0,count=0;
+		int len = 0, idx = 0, count = 0;
 		HashSet<Character> set = new HashSet<Character>();
-		while(idx < str.length()) {
-			if(!set.contains(str.charAt(idx))) {
+		while (idx < str.length()) {
+			if (!set.contains(str.charAt(idx))) {
 				set.add(str.charAt(idx));
 				count++;
 				idx++;
-				if(count >= len) {
+				if (count >= len) {
 					len = count;
 				}
-			}else {
+			} else {
 				set.clear();
-				if(count >=len) {
+				if (count >= len) {
 					len = count;
 				}
-				count=0;
+				count = 0;
 			}
 		}
-		
+
 		return len;
 	}
 
