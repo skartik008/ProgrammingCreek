bdfd4ca9344776a751b7b019b4b0a0c4a27bfa9c
Kartik Siddhabathula <ksiddhabathula@apple.com>
2019-07-30 22:25:27 -0700

    Program to find the shortest transformation distance between two words

--- ArraysStrings/WordLadder.java
+++ ArraysStrings/WordLadder.java
@@ -1,4 +1,4 @@
-package programcreek.Arrays;
+package programcreek.ArraysStrings;
 
 /**
  * 
@@ -13,6 +13,7 @@ import java.util.Set;
 class WordNode {
 	String word;
 	int numSteps;
+
 	public WordNode(String word, int numSteps) {
 		this.word = word;
 		this.numSteps = numSteps;
@@ -31,41 +32,41 @@ public class WordLadder {
 		dict.add("dog");
 		dict.add("lot");
 		dict.add("log");
-		
-		int res = ladderLength(start,end,dict);
+
+		int res = ladderLength(start, end, dict);
 		System.out.println(res);
 	}
 
 	private static int ladderLength(String start, String end, Set<String> dict) {
 		LinkedList<WordNode> queue = new LinkedList<WordNode>();
-		queue.add(new WordNode (start,1));
+		queue.add(new WordNode(start, 1));
 		dict.add(end);
-		
-		while(!queue.isEmpty()) {
+
+		while (!queue.isEmpty()) {
 			WordNode top = queue.remove();
 			String word = top.word;
-			
-			if(word.equals(end)) {
+
+			if (word.equals(end)) {
 				return top.numSteps;
 			}
 			char[] arr = word.toCharArray();
-            for(int i=0; i<arr.length; i++){
-                for(char c='a'; c<='z'; c++){
-                    char temp = arr[i];
-                    if(arr[i]!=c){
-                        arr[i]=c;
-                    }
- 
-                    String newWord = new String(arr);
-                    if(dict.contains(newWord)){
-                        queue.add(new WordNode(newWord, top.numSteps+1));
-                        dict.remove(newWord);
-                    }
- 
-                    arr[i]=temp;
-                }
-            }
-		}	
+			for (int i = 0; i < arr.length; i++) {
+				for (char c = 'a'; c <= 'z'; c++) {
+					char temp = arr[i];
+					if (arr[i] != c) {
+						arr[i] = c;
+					}
+
+					String newWord = new String(arr);
+					if (dict.contains(newWord)) {
+						queue.add(new WordNode(newWord, top.numSteps + 1));
+						dict.remove(newWord);
+					}
+
+					arr[i] = temp;
+				}
+			}
+		}
 		return 0;
 	}
 }
