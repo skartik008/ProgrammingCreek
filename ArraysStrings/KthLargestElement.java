b32e1f97c6568f2f4649a9b16d64dfbcbc585719
Kartik Siddhabathula <ksiddhabathula@apple.com>
2019-08-01 08:43:05 -0700

    Program to return the Kth largest element in an unsorted array

---
+++ ArraysStrings/KthLargestElement.java
@@ -0,0 +1,32 @@
+package programcreek.ArraysStrings;
+
+/**
+ * 
+ * @author kartik
+ * Program to return the Kth largest element in an unsorted array
+ */
+
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+
+	public static void main(String[] args) {
+		int[] arr = {3,2,1,5,4,6,7};
+		int k = 3;
+		int result = findKthLargest(arr,k);
+		System.out.println(result);
+
+	}
+
+	private static int findKthLargest(int[] arr, int k) {
+		PriorityQueue<Integer> q = new PriorityQueue<Integer>(k);
+		for(int i=0;i<arr.length;i++) {
+			q.offer(arr[i]);
+			if(q.size() > k) {
+				q.poll();
+			}
+		}	
+		return q.peek();
+	}
+
+}
