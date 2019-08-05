eb208932ccf72614653a49b482dc3bed29d84719
Kartik Siddhabathula <ksiddhabathula@apple.com>
2019-08-04 17:20:48 -0700

    Program to determine whether two given strings are isomorphic

--- ArraysStrings/IsomorphicStrings.java
+++ ArraysStrings/IsomorphicStrings.java
@@ -8,39 +8,38 @@ package programcreek.ArraysStrings;
 
 import java.util.HashMap;
 
-
 public class IsomorphicStrings {
 
 	public static void main(String[] args) {
-		String one = "ego";
+		String one = "egg";
 		String two = "add";
-		
-		Boolean result = isomorphicCheck(one,two);
+
+		Boolean result = isomorphicCheck(one, two);
 		System.out.println(result);
 	}
 
 	private static Boolean isomorphicCheck(String one, String two) {
-		if(one.length() != two.length()) {
+		if (one.length() != two.length()) {
 			return false;
 		}
-		HashMap<Character,Character> mapOne = new HashMap<Character,Character>();
-		HashMap<Character,Character> mapTwo = new HashMap<Character,Character>();
-		for(int i=0;i<one.length();i++) {
+		HashMap<Character, Character> mapOne = new HashMap<Character, Character>();
+		HashMap<Character, Character> mapTwo = new HashMap<Character, Character>();
+		for (int i = 0; i < one.length(); i++) {
 			char c1 = one.charAt(i);
 			char c2 = two.charAt(i);
-			if(mapOne.containsKey(c1)) {
-				if(c2!=mapOne.get(c1)) {
+			if (mapOne.containsKey(c1)) {
+				if (c2 != mapOne.get(c1)) {
 					return false;
 				}
-			}else {
-				if(mapTwo.containsKey(c2)) {
+			} else {
+				if (mapTwo.containsKey(c2)) {
 					return false;
 				}
 				mapOne.put(c1, c2);
 				mapTwo.put(c2, c1);
 			}
 		}
-		
+
 		return true;
 	}
 
