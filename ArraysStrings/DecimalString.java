1b76314d99403550352a3a1721c4d109af1475ce
Kartik Siddhabathula <ksiddhabathula@apple.com>
2019-10-15 09:08:27 -0700

    Print all decimal strings of given input size

---
+++ ArraysStrings/DecimalString.java
@@ -0,0 +1,25 @@
+package programcreek.ArraysStrings;
+
+/**
+ * 
+ * @author kartik
+ * Print all decimal strings of given input size
+ */
+
+public class DecimalString {
+
+	public static void main(String[] args) {
+		int input = 2;
+		dsHelper("",input);
+	}
+
+	private static void dsHelper(String string, int input) {
+		if(input == 0) {
+			System.out.println(string);
+		}else {
+			for(int i=0;i<10;i++) {
+				dsHelper(string+i,input-1);
+			}
+		}		
+	}
+}
