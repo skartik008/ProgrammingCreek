ad92d599b72a6d7f0c862430ce2b9963f3c9c8d3
Kartik Siddhabathula <ksiddhabathula@apple.com>
2019-10-15 13:04:52 -0700

    Program to reverse words in a string

--- ArraysStrings/ReverseWordsString.java
+++ ArraysStrings/ReverseWordsString.java
@@ -2,25 +2,24 @@ package programcreek.ArraysStrings;
 
 /**
  * 
- * @author kartik
- *	Program to reverse words in a string
+ * @author kartik Program to reverse words in a string
  */
 
 public class ReverseWordsString {
 
 	public static void main(String[] args) {
 		String str = "string a in words reverse to Program";
-		System.out.println(reverseWords(str));		
+		System.out.println(reverseWords(str));
 	}
 
 	private static String reverseWords(String str) {
 		StringBuilder result = new StringBuilder();
-		int i = str.length()-1;
-		int start,end =i+1;
-		while(i>=0) {
-		if(str.charAt(i) == ' ') {
-				start = i+1;
-				while (start!=end) {
+		int i = str.length() - 1;
+		int start, end = i + 1;
+		while (i >= 0) {
+			if (str.charAt(i) == ' ') {
+				start = i + 1;
+				while (start != end) {
 					result.append(str.charAt(start++));
 				}
 				result.append(' ');
@@ -29,10 +28,10 @@ public class ReverseWordsString {
 			i--;
 		}
 		start = 0;
-		while(start!=end) {
+		while (start != end) {
 			result.append(str.charAt(start++));
 		}
 		return result.toString();
 	}
-	
+
 }
