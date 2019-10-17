f5fd661c024e1b6a43a3b8b5d5d2f2a591684900
Kartik Siddhabathula <ksiddhabathula@apple.com>
2019-10-16 19:20:29 -0700

    Given a node find the number of possible binary search trees

---
+++ TreeHeapTrie/NumberBST.java
@@ -0,0 +1,29 @@
+package programcreek.TreeHeapTrie;
+
+/**
+ * 
+ * @author kartik
+ * Given a node find the number of possible binary search trees
+ */
+
+public class NumberBST {
+
+	public static void main(String[] args) {
+		int input = 3;
+		long res = numberBST(input);
+		System.out.println(res);
+	}
+
+	private static long numberBST(int input) {
+		if(input == 0) {
+			return 1;
+		}
+		long BST = 0;
+		for(int leftSub = 0; leftSub < input; leftSub++) {
+			int rightSub = input - 1 - leftSub;
+			BST += numberBST(leftSub) * numberBST(rightSub);
+		}
+		return BST;
+	}
+
+}
