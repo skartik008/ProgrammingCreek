3480a4548046076e9a55a8b972bd1d25bc890b2a
Kartik Siddhabathula <ksiddhabathula@apple.com>
2019-10-19 10:46:37 -0700

    Program to generate palindromic decompositions of a string

---
+++ ArraysStrings/PalindromicDecompositionString.java
@@ -0,0 +1,62 @@
+package programcreek.ArraysStrings;
+
+/**
+ * @author kartik
+ * Program to generate palindromic decompositions of a string
+ */
+
+import java.util.ArrayList;
+
+public class PalindromicDecompositionString {
+
+	public static void main(String[] args) {
+		String str = "abracadabra";
+
+		String res[];
+		res = generate_palindromic_decompositions(str);
+		for (int i = 0; i < res.length; i++) {
+			System.out.println(res[i]);
+		}
+	}
+
+	private static String[] generate_palindromic_decompositions(String str) {
+		ArrayList<String> arr = new ArrayList<String>();
+		int n = str.length();
+		helper(arr, str, 0, n, "");
+		String[] ans = GetStringArray(arr);
+		return ans;
+	}
+
+	private static void helper(ArrayList<String> arr, String str, int pos, int n, String curr_string) {
+		if (pos == n) {
+			arr.add(curr_string);
+			return;
+		}
+		for (int i = pos; i < n; i++) {
+			if (isPalindrome(str, pos, i)) {
+				if (pos == 0) {
+					helper(arr, str, i + 1, n, str.substring(pos, i+ 1));
+				} else {
+					helper(arr, str, i + 1, n, curr_string + '|' + str.substring(pos, i+1));
+				}
+			}
+		}
+	}
+
+	private static boolean isPalindrome(String str, int l, int r) {
+		while (l < r) {
+			if (str.charAt(l++) != str.charAt(r--)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static String[] GetStringArray(ArrayList<String> res) {
+		String str[] = new String[res.size()];
+		for (int i = 0; i < res.size(); i++) {
+			str[i] = res.get(i);
+		}
+		return str;
+	}
+}
