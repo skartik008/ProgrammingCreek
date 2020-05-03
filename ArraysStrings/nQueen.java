30bbadd05254fb09f4e15c97d5aa1c7d6fc3305a
Kartik Siddhabathula <ksiddhabathula@apple.com>
2020-05-03 10:27:20 -0700

    nQueen.java

---
+++ ArraysStrings/nQueen.java
@@ -0,0 +1,65 @@
+package programcreek.ArraysStrings;
+
+/**
+ * 
+ * @author kartik
+ * nQueen problem
+ */
+
+public class nQueen {
+
+	   /***************************************************************************
+	    * Return true if queen placement q[n] does not conflict with
+	    * other queens q[0] through q[n-1]
+	    ***************************************************************************/
+	    public static boolean isConsistent(int[] q, int n) {
+	        for (int i = 0; i < n; i++) {
+	            if (q[i] == q[n])             return false;   // same column
+	            if ((q[i] - q[n]) == (n - i)) return false;   // same major diagonal
+	            if ((q[n] - q[i]) == (n - i)) return false;   // same minor diagonal
+	        }
+	        return true;
+	    }
+
+	   /***************************************************************************
+	    * Prints n-by-n placement of queens from permutation q in ASCII.
+	    ***************************************************************************/
+	    public static void printQueens(int[] q) {
+	        int n = q.length;
+	        for (int i = 0; i < n; i++) {
+	            for (int j = 0; j < n; j++) {
+	                if (q[i] == j) System.out.print("Q ");
+	                else           System.out.print("* ");;
+	            }
+	            System.out.println();
+	        }  
+	        System.out.println();
+	    }
+
+
+	   /***************************************************************************
+	    *  Try all permutations using backtracking
+	    ***************************************************************************/
+	    public static void enumerate(int n) {
+	        int[] a = new int[n];
+	        enumerate(a, 0);
+	    }
+
+	    public static void enumerate(int[] q, int k) {
+	        int n = q.length;
+	        if (k == n) printQueens(q);
+	        else {
+	            for (int i = 0; i < n; i++) {
+	                q[k] = i;
+	                if (isConsistent(q, k)) enumerate(q, k+1);
+	            }
+	        }
+	    }  
+
+
+	    public static void main(String[] args) {
+	        int n = 4;
+	        enumerate(n);
+	    }
+
+	}
