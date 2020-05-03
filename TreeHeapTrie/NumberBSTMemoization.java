19cf1c46d1fa98b739b0116789b90d392a53f2bd
Kartik Siddhabathula <ksiddhabathula@apple.com>
2020-05-03 10:12:15 -0700

    Given a node find the number of possible binary search trees using Memoization

--- TreeHeapTrie/NumberBSTMemoization.java
+++ TreeHeapTrie/NumberBSTMemoization.java
@@ -2,40 +2,43 @@ package programcreek.TreeHeapTrie;
 
 /**
  * 
- * @author kartik Given a number of nodes, find the no of Binary Search Trees that can be constructed with that nodes using Memoization
- *        
+ * @author kartik Given a number of nodes, find the no of Binary Search Trees
+ *         that can be constructed with that nodes using Memoization
+ * 
  */
 
 public class NumberBSTMemoization {
-	static int MAX_INT = 30;
-	static long memArray[] = new long[MAX_INT];
-	
+	static int MAX_INT = 50;
+	static long memArray[] = new long[MAX_INT + 1];
+
 	public static void main(String[] args) {
-		for(int i=0;i<MAX_INT;i++) {
+		
+		for (int i = 0; i <= MAX_INT; i++) {
 			memArray[i] = -1;
 		}
 		long res;
-		int input = 4;
+		int input = 30;
 		res = bstNumber(input);
 		System.out.println(res);
 		System.out.println();
 	}
 
 	private static long bstNumber(int input) {
-		if(input > MAX_INT) {
+		if (input > MAX_INT || input < 0) {
 			return -1;
 		}
 		if (input == 0) {
+			memArray[0] = 1;
 			return 1;
 		}
-		if(memArray[input] != -1) {
+		if (memArray[input] != -1) {
 			return memArray[input];
 		}
 		long BST = 0;
 		for (int no_nodes_left_BST = 0; no_nodes_left_BST < input; no_nodes_left_BST++) {
 			int no_nodes_right_BST = input - 1 - no_nodes_left_BST;
 			BST += bstNumber(no_nodes_left_BST) * bstNumber(no_nodes_right_BST);
-			
+
 		}
 		memArray[input] = BST;
 		return BST;
