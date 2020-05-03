83b0815d677b0833c1afaba31df2e8c94b1e63f8
Kartik Siddhabathula <ksiddhabathula@apple.com>
2020-05-03 10:04:11 -0700

    Program to find the length of the last word in a given string

--- ArraysStrings/LengthOfLastWord.java
+++ ArraysStrings/LengthOfLastWord.java
@@ -2,14 +2,13 @@ package programcreek.ArraysStrings;
 
 /**
  * 
- * @author kartik
- *	Program to find the length of the last word in a given string
+ * @author kartik Program to find the length of the last word in a given string
  */
 
 public class LengthOfLastWord {
 
 	public static void main(String[] args) {
-		String str = "Hello World";
+		String str = "Kartik Siddhabathula";
 		int res = lengthWord(str);
 		System.out.println(res);
 	}
@@ -19,10 +18,9 @@ public class LengthOfLastWord {
 			return 0;
 		}
 		str = str.trim();
-		char[] c = str.toCharArray();
-		int i = c.length - 1;
+		int i = str.length() - 1;
 		int len = 0;
-		while (i >= 0 && c[i] != ' ') {
+		while (i >= 0 && str.charAt(i) != ' ') {
 			len++;
 			i--;
 		}
