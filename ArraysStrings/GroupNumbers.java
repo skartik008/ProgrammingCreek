e305a9ef68598a8c62e2a9e15c22a9afcb86eed5
Kartik Siddhabathula <ksiddhabathula@apple.com>
2020-05-03 10:02:56 -0700

    Group an array of integers, group the numbers into even and odd

--- ArraysStrings/GroupNumbers.java
+++ ArraysStrings/GroupNumbers.java
@@ -2,14 +2,14 @@ package programcreek.ArraysStrings;
 
 /**
  * 
- * @author kartik Group an array into evens and odds i.e. rearrange the array in
+ * @author kartik Group an array of integers, group the numbers into even and odd i.e. rearrange the array in
  *         place into even and odd
  */
 
 public class GroupNumbers {
 
 	public static void main(String[] args) {
-		int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
 		int[] res = new int[arr.length];
 		res = oddEvenGroup(arr);
 		for (int i = 0; i < res.length; i++) {
@@ -23,16 +23,19 @@ public class GroupNumbers {
 		int rightPtr = arr.length - 1;
 
 		while (leftPtr <= rightPtr) {
-			if (arr[rightPtr] % 2 == 0) {
-				int temp = arr[rightPtr];
-				arr[rightPtr] = arr[leftPtr];
-				arr[leftPtr] = temp;
+			if (arr[rightPtr] % 2 != 0) {
+				swap(arr,rightPtr,leftPtr);
 				leftPtr++;
 			} else {
 				rightPtr--;
 			}
 		}
 		return arr;
+	}
+	private static void swap(int[] array, int rightIdx, int leftIdx) {
+		int temp = array[leftIdx];
+		array[leftIdx] = array[rightIdx];
+		array[rightIdx] = temp;
 	}
 
 }
