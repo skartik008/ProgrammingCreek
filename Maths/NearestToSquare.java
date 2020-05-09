da9334069dba85cbcf0dba3a2871a1f92be55d89
Kartik Siddhabathula <ksiddhabathula@apple.com>
2020-05-09 11:54:35 -0700

    Program to return the number who's square is closest to the given number i.e. difference between the given number and the returned numbers is the smallest

---
+++ Maths/NearestToSquare.java
@@ -0,0 +1,41 @@
+package programcreek.Maths;
+
+/**
+ * 
+ * @author kartik
+ * Program to return the number who's square is closest to the given number i.e. difference between the given number and the returned numbers is the smallest
+ *
+ */
+
+public class NearestToSquare {
+
+	public static void main(String[] args) {
+		int number = 90;
+		int result = helper(number);
+		System.out.println(result);
+	}
+
+	private static int helper(int number) {
+		if (number < 1) {
+			return 0;
+		} else if (number == 1) {
+			return 1;
+		}
+		for (int i = 1; i < number / 2; i++) {
+			int square = i * i;
+			if (square == number) {
+				return 0;
+			} else if (square > number) {
+				int prevNumSquare = (i - 1) * (i - 1);
+				int diff1 = square - number;
+				int diff2 = number - prevNumSquare;
+				if(diff1 > diff2 ) {
+					return i-1;
+				}else {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+}
